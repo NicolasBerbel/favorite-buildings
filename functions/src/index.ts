ded04cf6a160e7674474559d83e2d01fa5ba3cf7
Nícolas Berbel <nicolas.berbel@gmail.com>
2020-03-10 17:45:42 -0300

    Setups /buildings function.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,29 @@
 import * as functions from 'firebase-functions';
+import axios from 'axios';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+const { api_url : apiUrl, api_token : apiToken } = functions.config().orulo;
+const api = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    Authorization: `Bearer ${apiToken}`
+  }
+});
+
+export const buildings = functions.https.onRequest(async (req, res) => {
+  try {
+    const { data } = await api.get('/buildings', {
+      params: {
+        page: req.query.page
+      }
+    });
+
+    return res.json( data );
+  } catch ( error ) {
+    const { status, data : { error_message } } = error.response;
+
+    return res.status(status).json({
+      status,
+      error_message
+    })
+  }
 });
